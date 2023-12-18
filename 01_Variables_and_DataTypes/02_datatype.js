9f6c06714c78dc0dba1f38fd43ca0d5a5040c71b
anil <Anil-Gehlot>
2023-12-18 17:25:54 +0530

    datatypes in JavaScript

---
+++ 01_Variables_and_DataTypes/02_datatype.js
@@ -0,0 +1,41 @@
+/*
+
+Primitive Types : Number, String, Boolean, Undefined, Null
+*/
+
+// Number Data Type
+let var1 = 23;
+console.log( typeof(var1) );
+
+let var2 = 345.45;
+console.log( typeof(var2) );
+
+
+
+// String Data Type
+let var3 = "anil";
+console.log( typeof(var3) )
+
+let var4 = 'a';
+console.log( typeof(var4) )
+
+let var5 = "Hii Welcome to my world...";
+console.log( typeof(var5) )
+
+
+// Boolean Data Type
+let var6 = true;
+console.log( typeof(var6) )
+
+let var7 = false;
+console.log( typeof(var7) )
+
+
+// Undefined Data Type
+let var8;
+console.log(var8)
+
+
+// Null Data Type
+let var9 = null
+console.log( typeof(var9) )
