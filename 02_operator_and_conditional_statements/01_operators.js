4a429d544512823790f5765127e9a865240628f2
anil <Anil-Gehlot>
2023-12-19 21:38:48 +0530

    operators in JavaScript

---
+++ 02_operator_and_conditional_statements/01_operators.js
@@ -0,0 +1,32 @@
+
+
+//  This is single line comments.
+
+/* This is multi line
+     comments .     
+*/
+
+
+/*
+Assignment Operators :  =,  +=,  -=,  *=,  %=,  **=
+
+Comparison Operators : ==, ===, !+ , !==, <, >, <=, >=
+
+        == in JavaScript is used for comparing two variables, but it ignores the datatype of variable. 
+        === is used for comparing two variables, but this operator also checks datatype and compares two values
+
+Logical Operators  : Logical AND &&, Logical OR ||, Logical NOT !
+
+Ternary Operators : condition ? true output : false output
+
+unary operator : ++ , --
+
+*/
+
+let var1 = 43;
+let var2 = '43';
+
+// let input = prompt("hii enter ");
+
+console.log(var1 == var2) // it checks only value hence : true
+console.log(var1 === var2) // it checks value as well as  datatype also hjence : false
