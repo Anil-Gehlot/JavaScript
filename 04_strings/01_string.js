92787397f12736df6168eb2262c60b74f88618b2
anil <Anil-Gehlot>
2023-12-19 21:42:22 +0530

    ways of string declaration

---
+++ 04_strings/01_string.js
@@ -0,0 +1,23 @@
+
+
+// A JavaScript string is zero or more characters written inside quotes.
+// String is a sequence of characters used to represent text.
+
+
+let str1 = 'hiii i am anil gehlot.'               // with single quote
+console.log(str1)
+
+
+let str2 = "hello!! world.....";                // with dual quote
+console.log(str2)
+
+
+let str3 = `This is Template literals.`;      // with backtick : also known as Template literals.
+console.log(str3)
+
+
+// length : it is a property of string tell the length of string.
+console.log(str1.length);
+console.log(str2.length);
+console.log(str3.length);
+
