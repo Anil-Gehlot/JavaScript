b312e0e6556569e407b9740f0298af89d33fcc51
anil <Anil-Gehlot>
2023-12-25 09:33:05 +0530

    functions with parameter and without parameter

---
+++ 06_functions_and_methods.js/01_function_declaration.js
@@ -0,0 +1,19 @@
+
+// functions without parameter
+function myFunction(){
+    return "This is my function."
+}
+
+// Calling function
+functionCall = myFunction();    
+console.log(functionCall)
+
+
+
+
+//  function with parameter
+function addTwoNumbers(num1, num2){
+    return num1+num2
+}
+
+console.log( addTwoNumbers(2,3) )
